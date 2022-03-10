fa8b8392cc8912cdb8d1ce3dbe0b71fecdfe9987
김소영(SOYEONG) <thdudmm@gmail.com>
2022-03-10 16:59:39 +0900

    ajax lecture(7) - gson

---
+++ chap07-gson/src/com/greedy/model/dto/MemberDTO.java
@@ -0,0 +1,60 @@
+package com.greedy.model.dto;
+
+public class MemberDTO {
+	
+	private String code;
+	private String firstName;
+	private String lastName;
+	private int age;
+	
+	public MemberDTO() {}
+
+	public MemberDTO(String code, String firstName, String lastName, int age) {
+		super();
+		this.code = code;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "MemberDTO [code=" + code + ", firstName=" + firstName + ", lastName=" + lastName + ", age=" + age + "]";
+	}
+
+	
+	
+
+}
