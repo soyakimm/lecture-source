86fc344b2822f185528e336a8c8d8af9c8a582a9
김소영(SOYEONG) <thdudmm@gmail.com>
2022-03-15 17:46:14 +0900

    mybatis lecture(2)

---
+++ chap02-scope-lifecycle/src/com/greedy/section01/xmlconfig/Application.java
@@ -0,0 +1,17 @@
+package com.greedy.section01.xmlconfig;
+
+import static com.greedy.section01.xmlconfig.Template.getSqlSession;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		System.out.println(getSqlSession());
+		System.out.println(getSqlSession());
+		System.out.println(getSqlSession());
+		System.out.println(getSqlSession());
+		System.out.println(getSqlSession());
+		
+	}
+
+}
